84895250daac6132f8c965645bc0e55fdc60bba3
zzzezzzo <157899258+zzzezzzo@users.noreply.github.com>
2024-12-26 19:22:45 +0200

    Update master.blade.php
    this update the master layout

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -7,7 +7,7 @@
     {{-- <link rel="stylesheet" href="{{ asset('build/assets/app-B2VwMapJ.css') }}"> --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/css/app.css') }}">
     <link rel="stylesheet" href="http://findteam-production.up.railway.app/css/app.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
